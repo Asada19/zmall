01e8b4d9f74dad850e354c7054841b1a0bd1ddb3
Rakhat Zhumaev <livencor@gmail.com>
2023-01-20 16:18:48 +0600

    statistic view and comment added

--- advertisement/permissions.py
+++ advertisement/permissions.py
@@ -8,3 +8,16 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
             return True
 
         return obj.owner == request.user
+
+
+class IsCommentOwner(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_superuser:
+            return True
+        return obj.user == request.user
